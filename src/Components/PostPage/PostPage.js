809ac216abed9317d876d1834e8a4f1394fd0935
koziolek151815 <koziolek151815x@gmail.com>
2021-05-18 17:28:02 +0200

    adding replies to posts

--- src/Components/PostPage/PostPage.js
+++ src/Components/PostPage/PostPage.js
@@ -3,15 +3,21 @@ import React, {useEffect, useState} from "react";
 import axios, * as others from 'axios';
 import {Link} from "react-router-dom";
 import './PostPage.css';
+import Comment from "../Comment/Comment";
 
 function PostPage(props) {
     const [post, setPost] = useState([]);
     const [username, setUsername] = useState('');
+    const [comments, setComments] = useState([]);
     const token = localStorage.getItem('token');
     useEffect(async () => {
         const response = await axios.get(
             'http://localhost:8081/posts/getById?postId=' +props.match.params.id, { headers: {"Authorization" : `Bearer ${token}`} }
         );
+        const responseComments = await axios(
+            'http://localhost:8081/posts/getPostReplies?postId=' +props.match.params.id, { headers: {"Authorization" : `Bearer ${token}`} }
+        );
+        setComments(responseComments.data.content);
         console.log(response.data.postAuthor);
         setPost(response.data);
         setUsername(response.data.postAuthor.username);
@@ -20,7 +26,7 @@ function PostPage(props) {
     function formatDate(dateParam){
         const date = new Date(dateParam);
         const day = ("0" + date.getDate()).slice(-2);
-        const month = ("0" + date.getMonth()).slice(-2);
+        const month = ("0" + (date.getMonth()+1)).slice(-2);
         const year = date.getFullYear();
         const hours = ("0" + date.getHours()).slice(-2);
         const minutes = ("0" + date.getMinutes()).slice(-2);
@@ -39,6 +45,9 @@ function PostPage(props) {
                 <p> Author: {username}</p>
                 <br/>
             </div>
+            {comments.map(comment =>
+                <Comment comment = {comment} key={comment.id} />
+            )}
         </div>
     );
 }
