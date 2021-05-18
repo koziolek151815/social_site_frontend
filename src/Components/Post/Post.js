93ab41f1b5bb68fb517a004e7ce675dfacdc04de
Sebastian <sebastian.korytkowski@student.wat.edu.pl>
2021-05-18 22:20:28 +0200

    Image fix

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -7,18 +7,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import {formatDate} from "../../Utility/Date";
+import {Image} from "react-bootstrap";
 
 function Post(props) {
 
     const token = localStorage.getItem('token');
     useEffect(async () => {
 
+        const imageHolder = document.getElementById("imageHolder" + props.post.postId);
+
         if(props.post.postPhotoName!=null) {
             const response = await axios.get(
                 'http://localhost:8081/posts/getPhoto?postId=' + props.post.postId, {headers: {"Authorization": `Bearer ${token}`}}
             );
-            document.getElementById("image" + props.post.postId).src = "data:image/jpeg;base64," + response.data;
+
+            imageHolder.innerHTML = `<img id=\"image${props.post.postId}\" alt=\"post img\" src=\"data:image/jpeg;base64,${response.data}\" className=\"align-content-center img-fluid thumb margin10 img-thumbnail\"/>`
+
         }
+        else
+        {
+            imageHolder.innerText = ""
+        }
+
+
 
     }, []);
 
@@ -27,10 +38,10 @@ function Post(props) {
             <div className="col-md-12 py-2 blogShort">
                 <h1>{props.post.title}</h1>
                 <span className="float-left"> Author: {props.post.postAuthor.username}</span>
-                <span className="float-right">{formatDate(props.post.postCreatedDate)}</span>
-                <img id={"image" + props.post.postId} alt="post img" src="" className="align-content-center img-fluid thumb margin10 img-thumbnail"/>
+                <span className="float-right">{formatDate(props.post.postCreatedDate)}</span><br/>
+                <div id = {"imageHolder" + props.post.postId}></div>
                 <article><p>
-                    Content: {props.post.description}
+                    {props.post.description}
                 </p></article>
                 <Vote postRating={props.post.rating} postId={props.post.postId}/>
                 <a className="btn btn-default float-right bg-info" href={`/posts/${props.post.postId}`}>Link</a>
