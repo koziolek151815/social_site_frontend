04689b973aeeb929ee287ad2005c099f3191a186
Sebastian <sebastian.korytkowski@student.wat.edu.pl>
2021-05-19 18:20:16 +0200

    Added ScrollablePostView.js made it possible to paginate front page results using
    https://www.npmjs.com/package/react-infinite-scroll-component

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,23 +1,10 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import Post from "../Post/Post";
+import ScrollablePostView from "../ScrollablePostView/ScrollablePostView";
 
 function MainPage() {
-    const [posts, setPosts] = useState([]);
-    const token = localStorage.getItem('token');
-    useEffect(async () => {
-        const response = await axios.get(
-            'http://localhost:8081/posts/getFrontPage', { headers: {"Authorization" : `Bearer ${token}`} }
-        );
-        setPosts(response.data.content);
-    }, []);
-
     return (
         <div className="App">
             <h2> Posts </h2>
-            {posts.map(post =>
-                <Post post = {post} key={post.postId}  />
-            )}
+            <ScrollablePostView endpoint="getFrontPage" sort="id,DESC"></ScrollablePostView>
         </div>
     );
 }
