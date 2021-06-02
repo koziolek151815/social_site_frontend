050738f7846afac440e2f5eff96b4792bfb2d1c6
Sebastian <sebastian.korytkowski@student.wat.edu.pl>
2021-06-02 14:54:26 +0200

    If you open a comment page it automatically redirects you to the parent page.

--- src/Components/PostPage/PostPage.js
+++ src/Components/PostPage/PostPage.js
@@ -20,19 +20,41 @@ class PostPage extends React.Component {
         this.token = localStorage.getItem('token');
     }
 
+    async loadComments(postId)
+    {
+        const responseComments = await axios.get(
+            process.env.REACT_APP_BACKEND_URL + '/posts/getPostReplies?postId=' + postId + "&sort=postCreatedDate,ASC", {headers: {"Authorization": `Bearer ${this.token}`}});
+        this.setState({
+            comments: responseComments.data.content
+        })
+    }
+
     async componentDidMount() {
         const config = {headers: {"Authorization": `Bearer ${this.token}`}}
 
-        const response = await axios.get(
-            process.env.REACT_APP_BACKEND_URL + '/posts/getById?postId=' + this.props.match.params.id, config);
+        await axios.get(process.env.REACT_APP_BACKEND_URL + '/posts/getById?postId=' + this.props.match.params.id, config)
+            .then(async (response)=>{
+                const post = response.data;
 
-        const responseComments = await axios.get(
-            process.env.REACT_APP_BACKEND_URL + '/posts/getPostReplies?postId=' + this.props.match.params.id + "&sort=postCreatedDate,ASC", config);
+                if(post.parentPost==null)
+                {
+                    this.setState({post: post})
+                    await this.loadComments(post.postId);
+                }
+                else //This is a comment
+                {
+                    const parentPostId = post.parentPost.postId;
+                    //Change the url to parent post
+                    window.history.replaceState(null, '', "/posts/" + parentPostId);
+                    //Load parent post
+                    axios.get(process.env.REACT_APP_BACKEND_URL + '/posts/getById?postId=' + parentPostId, config)
+                        .then(async (response)=>{
+                            this.setState({post: response.data})
+                            await this.loadComments(parentPostId);
+                        })
+                }
+        });
 
-        this.setState({
-            post: response.data,
-            comments: responseComments.data.content
-        })
     }
 
     render() {
@@ -47,10 +69,10 @@ class PostPage extends React.Component {
 
 
                 {
-                    this.state.replyVisible ?
+                    this.state.replyVisible&&this.state.post != null?
                         (
                             <h2>Write new reply</h2>,
-                                <AddPost parentPostId={this.props.match.params.id} showError={this.props.showError}
+                                <AddPost parentPostId={this.state.post.postId} showError={this.props.showError}
                                          showLink={false}/>
                         ) :
                         (<button className="btn btn-default text-white bg-dark" onClick={() => {
